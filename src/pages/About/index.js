9fdef9fa9b3d8bd569dc3b17701639ef06e02cac
Vinicius Nattan Silva Lemos <viniciusnattan14@gmail.com>
2022-03-07 22:51:40 -0300

    react-router-dom to navigate

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -12,7 +12,6 @@ function About() {
             <div className="aboutText">
                 <h1>[ About ]</h1>
                 <p>I am 29 yo and started coding during my Biotechnology degree, when I focused on Bioinfomatics and joined a MSc working with RNASeq. Programming was in my life for the most part, and with that in mind, I went for web development joining the Cubos Academy course, which led me to Ford Motor Company. Right now my skills are focused on React and Angular for front end frameworks and nodeJs for back end. I mainly program in JavaScript and TypeScript. Team work, organization, time management and communication are my main soft skills. More information can be seen on my CV below.</p>
-                {/* <a href="../../assets/data/ViniciusNattan-CV.pdf" download>[ Download CV ]</a> */}
                 <Link to="/CV">[ View CV ]</Link>
             </div>
             <img className="triangle" src={Polygon} alt="sdad" />
